66f2eeaa7dbbf973242920e50decdb20ee0c5c8f
louis.yi <louis.yi@qima.com>
2024-07-31 18:25:06 +0800

    add page part

--- src/main/java/com/icarus/sword/business/controller/UserController.java
+++ src/main/java/com/icarus/sword/business/controller/UserController.java
@@ -1,12 +1,11 @@
 package com.icarus.sword.business.controller;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.icarus.sword.business.entity.User;
 import com.icarus.sword.business.service.IUserService;
+import com.icarus.sword.core.page.PageRequest;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,5 +19,16 @@ public class UserController {
     @GetMapping
     public List<User> getAllUsers() {
         return userService.list();
+    }
+
+    @GetMapping("/page")
+    public Page<User> getUsersByPage(@RequestBody PageRequest pageRequest) {
+        return userService.getUserByPage(pageRequest);
+    }
+
+    @GetMapping("/page2")
+    public Page<User> getUsersByPage2(@RequestParam(value = "pageNo", required = false, defaultValue = "1") int pageNo,
+                                      @RequestParam(value = "pageSize", required = false, defaultValue = "20") int pageSize) {
+        return userService.getUserByPage2(pageNo, pageSize);
     }
 }
--- src/main/java/com/icarus/sword/business/service/impl/UserServiceImpl.java
+++ src/main/java/com/icarus/sword/business/service/impl/UserServiceImpl.java
@@ -1,11 +1,23 @@
 package com.icarus.sword.business.service.impl;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.icarus.sword.business.entity.User;
 import com.icarus.sword.business.mapper.UserMapper;
 import com.icarus.sword.business.service.IUserService;
+import com.icarus.sword.core.page.PageRequest;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
+
+    @Override
+    public Page<User> getUserByPage(PageRequest pageRequest) {
+        return this.page(new Page<>(pageRequest.getPageNo(), pageRequest.getPageSize()));
+    }
+
+    @Override
+    public Page<User> getUserByPage2(int pageNo, int pageSize) {
+        return this.page(new Page<>(pageNo, pageSize));
+    }
 }
