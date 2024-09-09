0aa071e6357e5f1e8e5a241596cfcc900a54abf9
louis.yi <louis.yi@qima.com>
2024-09-09 17:37:28 +0800

    test git commit user

--- sword-admin/src/main/java/com/sword/SwordApplication.java
+++ sword-admin/src/main/java/com/sword/SwordApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SwordApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(SwordApplication.class, args);
     }
